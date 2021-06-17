6def3e1fe392b087bdcc07436bf1469109f31b31
cursoswebt <cursoswebt@gmail.com>
2021-06-17 13:53:47 -0500

    Nuevos achivos

--- js/firebase.js
+++
@@ -1,9 +0,0 @@
-firebase.initializeApp({
-    apiKey: "AIzaSyCwR_DGPnnINHMqzffhLoF6hdnj9LmZYB0",
-    authDomain: "cursoweb39.firebaseapp.com",
-    projectId: "cursoweb39",
-    storageBucket: "cursoweb39.appspot.com",
-    messagingSenderId: "561186197046",
-    appId: "1:561186197046:web:3b87d13433b3d6e7c55297",
-    measurementId: "G-1Q1ZM6MP7N"
-});
